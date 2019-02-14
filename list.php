1de5c5ad191dc86307f4c5a8ed2b360b4f04301f
Maria <maria@proto366.haagahelia.amk>
2019-02-14 12:16:46 +0200

    delete button

--- list.php
+++ list.php
@@ -16,11 +16,14 @@ if($_SESSION['username'] == 'username') {
 			echo $row["city"] . " ";
 			echo $row["state"] . " ";
 			echo $row["country"] . " ";
+			echo "<a href=\"delete.php?id=" . $row["id"] . "\">DELETE</a>";
 			print("<br>");
 		}
 	} else {
 		echo "0 results";
 	}
+	print("<br><br>");
+
 	print("</html>");
 	mysqli_close($conn);
 } else {
